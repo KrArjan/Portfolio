09dac3b96e1f7dc4c41f57c3929e0a25fcf42460
KrArjan <krarjanofficial@gmail.com>
2026-04-04 20:07:37 +0530

    Update portfolio.config.js

--- config/portfolio.config.js
+++ config/portfolio.config.js
@@ -49,7 +49,7 @@ const SITE_DATA = {
    * CHANGE_ME: Update turnstileSiteKey with your public key.
    * ================================================ */
   security: {
-    turnstileSiteKey: '1x00000000000000000000AA', // CHANGE_ME: Cloudflare Turnstile Site Key
+    turnstileSiteKey: '2x00000000000000000000AB', // CHANGE_ME: Cloudflare Turnstile Site Key
   },
 
   /* ===================== STATS ===================== *
